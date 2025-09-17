4e547f11d7099654e1b581bb8e8b23f0ed711f5a
Alex Yarm <alexyarmBRO@gmail.com>
2025-09-17 13:46:25 +0400

    improve properties; add selenoid preload option

--- src/test/java/org/example/tests/ui/base/BaseTest.java
+++ src/test/java/org/example/tests/ui/base/BaseTest.java
@@ -2,7 +2,6 @@ package org.example.tests.ui.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.example.ui.common.PageProvider;
@@ -11,8 +10,6 @@ import org.example.ui.steps.base.UserSteps;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BaseTest {
 
@@ -21,7 +18,6 @@ public class BaseTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = PageURL.BASE_URL;
-//        Configuration.remote = "http://selenoid:4444/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(false)
